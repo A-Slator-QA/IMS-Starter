90bacaf30826da3782da49ebe9ed401cec140adb
A-Slator-QA <andrew.slator95@gmail.com>
2022-07-11 17:33:32 +0100

    created item/order tables. multi-item orders in pipeline

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,5 +1,3 @@
-drop schema ims;
-
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
 USE `ims` ;
@@ -9,4 +7,20 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+`id` INT(11) NOT NULL AUTO_INCREMENT,
+`title` varchar (20) NOT NULL,
+`price` float NOT NULL,
+PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`customer-id` int(11) NOT NULL,
+`product-id-1` int(11) NOT NULL,
+`total-cost` float NOT NULL,
+PRIMARY KEY (`id`),
+Foreign key (`customer-id`) references `ims`.`customers`(`id`)
 );
