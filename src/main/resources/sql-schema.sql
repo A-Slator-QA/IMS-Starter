f12cf1e3009b61f2267dc42a3f90e818dabf612a
A-Slator-QA <andrew.slator95@gmail.com>
2022-07-12 13:03:31 +0100

    Updated data types and names

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -12,15 +12,15 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
 `id` INT(11) NOT NULL AUTO_INCREMENT,
 `title` varchar (20) NOT NULL,
-`price` float NOT NULL,
+`price` double NOT NULL,
 PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
-`customer-id` int(11) NOT NULL,
-`product-id-1` int(11) NOT NULL,
-`total-cost` float NOT NULL,
+`customer_id` int(11) NOT NULL,
+`item_id_1` int(11) NOT NULL,
+`total_cost` double NOT NULL,
 PRIMARY KEY (`id`),
-Foreign key (`customer-id`) references `ims`.`customers`(`id`)
+Foreign key (`customer_id`) references `ims`.`customers`(`id`)
 );
